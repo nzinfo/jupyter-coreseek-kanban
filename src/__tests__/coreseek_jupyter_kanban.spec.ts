85daf5c8e3c4669ed5757619d4f827709e52b9cb
nzinfo <li.monan@gmail.com>
2024-12-26 00:23:10 +0800

    init from copier

---
+++ src/__tests__/coreseek_jupyter_kanban.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
+ */
+
+describe('@coreseek/jupyter-kanban', () => {
+  it('should be tested', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
